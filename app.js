97f81532e2a5101e5ab8a13d8312b179120ad84e
PSADMIN <psadmin@psadmins-air-8.attlocal.net>
2021-05-16 19:44:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// this is a number
+var num = 5
+
+// this is a string
+var num ="5"
+
+//this is a boolean
+var dog = true
+
+// this is a boolean
+var dog = false
+
+//this is an array
+var colors =["blue","red","yellow"]
+//console.log(colors)
+//console.log(colors[0])
+//console.log(colors[2])
+
+// this is an object
+var anbu={
+    name:"Anbu",
+    age:31,
+    isSleepy:true,
+    sayHello: function(){
+        console.log(this.name + " " + this.age)
+    }
+}
+anbu.sayHello()
